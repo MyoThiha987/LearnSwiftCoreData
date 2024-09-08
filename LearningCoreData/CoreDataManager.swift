588f07b4d7622de32a9f16b05d29c1c81ad68070
liam <luciferthiha881997@gmail.com>
2024-09-08 17:36:29 +0700

    implement : Integrate coredata

---
+++ LearningCoreData/CoreDataManager.swift
@@ -0,0 +1,24 @@
+//
+//  Persistence.swift
+//  LearningCoreData
+//
+//  Created by Myo Thiha on 08/09/2024.
+//
+
+import CoreData
+
+class CoreDataStack{
+    static let shared : CoreDataStack = CoreDataStack()
+    let persistanceContainer : NSPersistentContainer
+    
+    private init(){
+        persistanceContainer = NSPersistentContainer(name: "LearningCoreData")
+        persistanceContainer.loadPersistentStores { description, error in
+            if let error = error{
+                fatalError("Unable to initialize CoreData \(error) ")
+            }
+        }
+    }
+    
+     
+}
